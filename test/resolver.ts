8406a6361293d1e94af45ec68762726071d13c37
haistack <zztianhai@qq.com>
2023-03-06 19:48:15 +0800

    Add reverse case and deployment archives of testnet.

--- test/resolver.ts
+++ test/resolver.ts
@@ -7,12 +7,18 @@ async function main() {
   const registry = await ethers.getContract('LNSRegistry', owner)
   const domain = 'lambda.lamb'
   const resolver = await registry.resolver(namehash(domain))
-  console.log(`resolver of ${domain} ${resolver}`)
+  console.log(`resolver of ${domain} is ${resolver}`)
   const resolverContract = (
     await ethers.getContractAt('PublicResolver', resolver)
   ).connect(await ethers.getSigner(owner))
   const addr = await resolverContract['addr(bytes32)'](namehash(domain))
-  console.log(`${domain} ==>> ${addr}`)
+  console.log(`[resolve] ${domain} ==>> ${addr}`)
+
+  const reverseNode = addr.substring(2) + '.addr.reverse'
+  const reverseResolver = await registry.resolver(namehash(reverseNode))
+  console.log(`resolver of ${addr} is ${reverseResolver}`)
+  const name = await resolverContract.name(namehash(reverseNode))
+  console.log(`[reverse] ${addr} ==>> ${name}`)
 }
 
 main()
